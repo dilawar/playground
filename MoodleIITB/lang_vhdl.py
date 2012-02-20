71ccf73fd6fd732feed9208ecfd4d5b44151f5cc
dilawar <dilawar.iitb@gmail.com>
2012-02-20 07:46:33 +0530

    Removed a bug. It can now match end entity entityname patterns also.

--- MoodleIITB/lang_vhdl.py
+++ MoodleIITB/lang_vhdl.py
@@ -1,5 +1,5 @@
 import re
-import glob, os
+import glob, os, subprocess
 
 class VHDL:
 
@@ -11,8 +11,13 @@ class VHDL:
     def dirName(self, dir):
         self.srcDir = dir
 
-    def search_testbench(self, dir):
-        files = glob.glob(dir+'/*.vhd')
+    def compile_testbench(self, dir):
+        '''
+        This function compiles the test_bench in given directory.
+        
+        '''
+        os.chdir(dir)
+        files = glob.glob('*.vhd')
         for file in files:
                 f = open(file, "r")
                 while True:
@@ -22,28 +27,26 @@ class VHDL:
                         print "EOF"
                     if not self.src: break
                     # This will match the testbech entity in given file.
-                    m = re.search(r"entity\ +(\w+)\ +is[\ \n]+end\ +(\w*)[;]", self.src, re.I)
+                    m = re.search(r"entity\ +(\w+)\ +is[\ \n]+end\ +(\w*)\ *\w*[;]", self.src, re.I)
                     if m : 
-                        self.compile_testbench(m.group(1), f)
+                        test_bench = m.group(1)
+                        print("Testbench entity is {0}".format(test_bench))
+                        print("In file {0}".format(f.name))
+                        #subprocess.call(["ghdl", "-a", f.name])
+                        #subprocess.call(["ghdl", "-m", test_bench])
+                        #subprocess.call(["ghdl", "-r", test_bench, "--stop-time=500ns", \
+                        #    "--vcd=out.vcd"])
+
                     else : 
                         #print "No testbench."
                         pass
-
-    def compile_testbench(self, test_bench, file):
-        '''
-        This function compiles the test_bench in given directory.
-        
-        '''
-        print("Testbench entity is {0}".format(test_bench))
-        print("In file {0}".format(file.name))
-
 
 vhdl = VHDL()
 vhdl.dirName('/home/dilawar/Works/myrepo/Courses/2012_VLSIDesignLab/Lab session 1')
 print vhdl.srcDir
 if os.path.exists(vhdl.srcDir) :
     for x in os.walk(vhdl.srcDir):
-        vhdl.search_testbench(x[0])
+        vhdl.compile_testbench(x[0])
 else:
     print "Given path does not exists."
 
