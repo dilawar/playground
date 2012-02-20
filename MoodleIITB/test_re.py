b07ffbd6354cac7fee8a4a2d331b3fc6ed46dbb5
dilawar <dilawar.iitb@gmail.com>
2012-02-20 15:23:03 +0530

    fixed pattern.

--- MoodleIITB/test_re.py
+++ MoodleIITB/test_re.py
@@ -13,24 +13,26 @@ port(a: in bit_vector(7 downto 0);
 	s: out bit_vector(7 downto 0);
 	c: out bit);
 end component;'''
-
+#
+#m = re.search(r'''component\s+(\w+)\s+is\s+
+#                port\s*[(]
+#               ((\s*\w+[:]\s*(in|out)\s+\w+([(]\d+\s+\w+\s+\d+[)])*[;]*)+[)]\s*[;])
+#                \s+end\s+component\s*\w*[;]''', data, re.I | re.VERBOSE)
+#
+#
+#if m:
+#   # print m.group(0)
+#   # print m.group(1)
+#   # print m.group(2)
+#    pass
+#
 m = re.search(r'''component\s+(\w+)\s+is\s+
                 port\s*[(]
-               ((\s*\w+[:]\s*(in|out)\s+\w+([(]\d+\s+\w+\s+\d+[)])*[;]*)+[)]\s*[;])
-                \s+end\s+component\s*\w*[;]''', data, re.I | re.VERBOSE)
-
-
-if m:
-   # print m.group(0)
-   # print m.group(1)
-   # print m.group(2)
-    pass
-
-m = re.search(r'''component\s+(\w+)\s+is\s+
-                port\s*[(]
-                ((\s*\w+(\s*[,]\s*\w+)*[:]\s*(in|out)\s*\w+[;]*)*)\s*[)]\s*[;]
+                (\s*\w+(\s*[,]\s*\w+\s*)*\s*[:]\s*
+                (in|out)\s*\w+([(]\s*\d+\s*\w+\s*\d+\s*[)])*[;]*)*
+                \s*[)]\s*[;]
                 \s+end\s+component\s*\w*[;]'''
-                , data1, re.I | re.VERBOSE)
+                , data, re.I | re.VERBOSE)
 
 if m:
     print m.group(0)
