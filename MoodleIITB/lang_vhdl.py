adb519ab8efc4238e9e0409bf00ba323c6bedc4a
dilawar <dilawar.iitb@gmail.com>
2012-02-20 07:14:49 +0530

    It can locate test bench in given directory.

--- MoodleIITB/lang_vhdl.py
+++ MoodleIITB/lang_vhdl.py
@@ -11,22 +11,23 @@ class VHDL:
     def dirName(self, dir):
         self.srcDir = dir
 
-    def search_testbench(self):
-        files = glob.glob(self.srcDir+'*.vhd')
+    def search_testbench(self, dir):
+        files = glob.glob(dir+'/*.vhd')
         for file in files:
-            f = open(file, "r")
-            print f
-            while True:
-                try:
-                    self.src = f.read()
-                except EOFError:
-                    print "EOF"
-                if not self.src: break
-                # This will match the testbech entity in given file.
-                m = re.search(r"entity\ +(\w+)\ +is[\ \n]+end\ +(\w*)[;]", self.src, re.I)
-                if m : 
-                    self.compile_testbench(m.group(1), f)
-                else : pass
+                f = open(file, "r")
+                while True:
+                    try:
+                        self.src = f.read()
+                    except EOFError:
+                        print "EOF"
+                    if not self.src: break
+                    # This will match the testbech entity in given file.
+                    m = re.search(r"entity\ +(\w+)\ +is[\ \n]+end\ +(\w*)[;]", self.src, re.I)
+                    if m : 
+                        self.compile_testbench(m.group(1), f)
+                    else : 
+                        #print "No testbench."
+                        pass
 
     def compile_testbench(self, test_bench, file):
         '''
@@ -34,8 +35,15 @@ class VHDL:
         
         '''
         print("Testbench entity is {0}".format(test_bench))
-        print("In file {0}".format(file))
+        print("In file {0}".format(file.name))
+
 
 vhdl = VHDL()
-vhdl.dirName('./')
-vhdl.search_testbench()
+vhdl.dirName('/home/dilawar/Works/myrepo/Courses/2012_VLSIDesignLab/Lab session 1')
+print vhdl.srcDir
+if os.path.exists(vhdl.srcDir) :
+    for x in os.walk(vhdl.srcDir):
+        vhdl.search_testbench(x[0])
+else:
+    print "Given path does not exists."
+
