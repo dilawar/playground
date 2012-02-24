cfdf92b18b244920e5e699c7b50391fa8310bf7c
dilawar <dilawar.iitb@gmail.com>
2012-02-24 10:36:01 +0530

    Removed some bugs. Some left.

--- MoodleIITB/lang_vhdl.py
+++ MoodleIITB/lang_vhdl.py
@@ -1,5 +1,5 @@
 import re
-import io
+import io, shutil
 import glob, os, subprocess, sys
 
 class VHDL:
@@ -94,8 +94,11 @@ class VHDL:
         This function create a testbench 
         '''
         for comp_name, comp_expr in self.component.iteritems():
-            self.tb = io.StringIO()
-            self.tb.write(u'''
+
+            if not os.path.isdir('./AUTO_GEN'):
+                os.makedirs('./AUTO_GEN')
+            tb = open("./AUTO_GEN/auto_"+self.tbname+".ghdl", "a");
+            tb.write(u'''---
 -- This testbench is automatically generated using a python
 -- script.
 -- (c) Dilawar Singh, dilawar@ee.iitb.ac.in
@@ -109,45 +112,45 @@ ENTITY testbench IS
 end ENTITY testbench;
 
 ARCHITECTURE stimulus OF testbench IS\n\tCOMPONENT ''')
-            self.tb.write(unicode(comp_name)+u'\n')
-            self.tb.write(u'\tPORT ( \n')
-            self.tb.write(u'\t'+unicode(comp_expr)+u'\n')
-            self.tb.write(u'\t);\nEND COMPONENT;\n')
+            tb.write(unicode(comp_name)+u'\n')
+            tb.write(u'\tPORT ( \n')
+            tb.write(u'\t'+unicode(comp_expr)+u'\n')
+            tb.write(u'\t);\nEND COMPONENT;\n')
 
             # Attach signal.
             for (name,comp), (a, expr) in self.port.iteritems():
                 if comp == comp_name :
-                    self.tb.write(u'\tSIGNAL '+unicode(name)+u' : '+unicode(expr))
-                    self.tb.write(u';\n')
+                    tb.write(u'\tSIGNAL '+unicode(name)+u' : '+unicode(expr))
+                    tb.write(u';\n')
                 else:
                     pass
 
-            self.tb.write(u'BEGIN\n')
-            self.tb.write(u'\tdut : '+unicode(comp_name)+u' \n\tPORT MAP (');
+            tb.write(u'BEGIN\n')
+            tb.write(u'\tdut : '+unicode(comp_name)+u' \n\tPORT MAP (');
 
             for (name, comp), (a, expr) in self.port.iteritems():
                 if comp == comp_name :
-                    self.tb.write(unicode(name)+u', ')
+                    tb.write(unicode(name)+u', ')
                 else:
                     pass
         
             
-            pos = self.tb.tell()
-            self.tb.seek(pos-2)
-            self.tb.write(u' );\n')
-            self.tb.write(u'\n\ttest : PROCESS \n')
+            pos = tb.tell()
+            tb.seek(pos-2)
+            tb.write(u' );\n')
+            tb.write(u'\n\ttest : PROCESS \n')
 
             # Create variables.
             for (name,comp), (a, expr) in self.port.iteritems():
                 if comp == comp_name :
-                    self.tb.write(u'\t\tVARIABLE var_'+unicode(name.strip())+u' : '+unicode(expr))
-                    self.tb.write(u';\n')
+                    tb.write(u'\t\tVARIABLE var_'+unicode(name.strip())+u' : '+unicode(expr))
+                    tb.write(u';\n')
                 else:
                     pass
 
-            self.tb.write(u'\n\t\tfile vector_file : text is in \"'\
+            tb.write(u'\n\t\tfile vector_file : text is in \"'\
                     +unicode(self.tbname)+u'.txt\";\n')
-            self.tb.write(u'''
+            tb.write(u'''
 \t\tVARIABLE l : LINE;
 \t\tVARIABLE vector_time : TIME;
 \t\tVARIABLE r : REAL;
@@ -155,7 +158,7 @@ ARCHITECTURE stimulus OF testbench IS\n\tCOMPONENT ''')
 \t\tVARIABLE space : CHARACTER;
                     ''')
 
-            self.tb.write(u'''
+            tb.write(u'''
 \t\tBEGIN
 \t\tWHILE NOT endfile(vector_file) LOOP
 \t\t\treadline(vector_file, l);
@@ -169,17 +172,17 @@ ARCHITECTURE stimulus OF testbench IS\n\tCOMPONENT ''')
             # read other variables and use them as input.
             for (name,comp), (a, expr) in self.port.iteritems():
                 if comp == comp_name :
-                    self.tb.write(u'\t\t\tread(l, var_'+unicode(name.strip())+\
+                    tb.write(u'\t\t\tread(l, var_'+unicode(name.strip())+\
                             u', good_val);\n') 
-                    self.tb.write(u'\t\t\tASSERT good_val REPORT \"bad '+\
-                            unicode(name)+u' value\"\n;')
+                    tb.write(u'\t\t\tASSERT good_val REPORT \"bad '+\
+                            unicode(name)+u' value\";\n')
                 else:
                     pass
             # Assign these values to input ports.
             for (name,comp), (type, expr) in self.port.iteritems():
                 if comp == comp_name :
                     if type.strip() == 'in' :
-                        self.tb.write(u'\t\t\t'+unicode(name)+u' <= var_'+\
+                        tb.write(u'\t\t\t'+unicode(name)+u' <= var_'+\
                                 unicode(name.strip())+u';\n')
                 else:
                     pass
@@ -188,21 +191,17 @@ ARCHITECTURE stimulus OF testbench IS\n\tCOMPONENT ''')
             for (name,comp), (type, expr) in self.port.iteritems():
                 if comp == comp_name :
                     if type.strip() == 'out' :
-                        self.tb.write(u'\t\t\tASSERT var_'+unicode(name.strip())+\
+                        tb.write(u'\t\t\tASSERT var_'+unicode(name.strip())+\
                                 u' = '+unicode(name.strip())+\
                                 u' REPORT \"vector mismatch\"'+\
                                 u' SEVERITY WARNING;\n')
                 else:
                     pass
 
-            self.tb.write(u'\t\tEND LOOP;\n')
-            self.tb.write(u'\t\tASSERT false REPORT \"Test complete\";\n')
-            self.tb.write(u'\t\tWAIT;\n')
-            self.tb.write(u'\t\tEND PROCESS;\n')
-            self.tb.write(u'\tEND ARCHITECTURE;\n')
-            if not os.path.isdir('./AUTO_GEN'):
-                os.makedirs('./AUTO_GEN')
-            tb_file = open("./AUTO_GEN/auto_"+self.tbname+".vhd", "w");
-            tb_file.write(self.tb.getvalue())
+            tb.write(u'\t\tEND LOOP;\n')
+            tb.write(u'\t\tASSERT false REPORT \"Test complete\";\n')
+            tb.write(u'\t\tWAIT;\n')
+            tb.write(u'\t\tEND PROCESS;\n')
+            tb.write(u'\tEND ARCHITECTURE;\n')
 
 
