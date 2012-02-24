fff939d01481df23e3cc65e17aba3463c3359dbe
dilawar <dilawar.iitb@gmail.com>
2012-02-24 15:25:49 +0530

    We can now create additional testbenches in AUTO_GEN directories. To use them we must copy a test vector file in appropriate directory

--- MoodleIITB/lang_vhdl.py
+++ MoodleIITB/lang_vhdl.py
@@ -6,9 +6,8 @@ class VHDL:
 
     def __init__(self):
         self.tbname = ''
-        self.component = dict()
-        self.component_expr = u''
-        self.port = dict()
+        self.comp_name = ''
+        self.comp_expr = ''
         self.src = ''
         self.srcDir = ''
 
@@ -22,42 +21,53 @@ class VHDL:
         '''
         os.chdir(dir)
         files = glob.glob('*.vhd')
+        tbcount = 0;
         for file in files:
-                f = open(file, "r")
-                if f:
-                    try:
-                        self.src = f.read()
-                    except EOFError:
-                        print "EOF"
-                    if not self.src: break
-                    # This will match the testbech entity in given file.
-                    m = re.search(r"entity\ +(\w+)\ +is[\ \n]+end\ +(\w*)\ *\w*[;]", self.src, re.I)
-                    if m : 
-                        test_bench = m.group(1)
-                        self.tbname = test_bench
-                        self.get_ports(test_bench, self.src);
-                        self.create_testbench(self.component, self.port)
+            f = open(file, "r")
+            self.src = ''
+            if f:
+                try:
+                    self.src = f.read()
+                except EOFError:
+                    print "Can not open file."
+                if not self.src: break
+                # This will match the testbech entity in given file.
+                m = re.search(r'entity\s+(\w+)\s+is[\s\n]+end\s+(\w*)\s*\w*[;]'\
+                        , self.src, re.I)
+                if m : 
+                    tbcount = tbcount+1
+                    test_bench = m.group(1)
+                    print "Testbench {0} is found".format(test_bench)
+                    self.tbname = test_bench
 
-                        print("Compiling entity {0} using {1}".format(test_bench, cxx))
-                        #print("In file {0}".format(f.name))
-                        #print "cxx : {0}".format(cxx)
-                        if cxx == 'ghdl':
-                            vcdOption = "--vcd="+test_bench+".vcd"
-                            subprocess.call(["ghdl", "-a", f.name] \
-                                    , stdout=subprocess.PIPE)
-                            subprocess.call(["ghdl", "-m", test_bench] \
-                                    , stdout=subprocess.PIPE)
-                            subprocess.call(["ghdl", "-r" \
-                                    , test_bench, "--stop-time=1000ns" \
-                                    , vcdOption] \
-                                    , stdout=subprocess.PIPE)
-                        elif cxx == 'vsim' :
-                            pass
+                    self.get_ports(test_bench, self.src);
+                    print '\n\n** ADDING NEW TESTBENCHES **\n'
 
-                    else : 
+                    self.add_testbenches(self.component, self.port)
+                    #print(" |- Compiling {0} using {1}".format(test_bench, cxx))
+                    #print("In file {0}".format(f.name))
+                    #print "cxx : {0}".format(cxx)
+                    if cxx == 'ghdl':
+                        vcdOption = "--vcd="+test_bench+".vcd"
+                        subprocess.call(["ghdl", "-a", f.name] \
+                                , stdout=subprocess.PIPE)
+                        subprocess.call(["ghdl", "-m", test_bench] \
+                                , stdout=subprocess.PIPE)
+                        subprocess.call(["ghdl", "-r" \
+                                , test_bench, "--stop-time=1000ns" \
+                                , vcdOption] \
+                                , stdout=subprocess.PIPE)
+                    elif cxx == 'vsim' :
                         pass
 
+                else : 
+                    pass
+                    
+        print 'Total {0} testbenches are found in this dir.'.format(tbcount)
+
     def get_ports(self, test_bench, data):
+        self.port = dict()
+        self.component = dict()
         print "Getting port information for {0} in file {1}".format(test_bench, file)
         m = re.search(r'''component\s+(\w+)\s*(is)*\s+
                 port\s*[(]
@@ -68,9 +78,10 @@ class VHDL:
                 , data, re.I | re.VERBOSE)
 
         if m:
+            name = m.group(1)
             text = m.group(3)
-            self.component_expr = text
-            self.component[m.group(1)] = text
+            self.comp_expr = text
+            self.component[(name, test_bench)] = text
             for pExpr in text.split(';'):
                 if pExpr.strip() == '':
                     pass
@@ -89,20 +100,27 @@ class VHDL:
 
 
 
-    def create_testbench(self, component, port):
+    def add_testbenches(self, component, port):
         '''
-        This function create a testbench 
+        This function adds new testbenches.
         '''
-        for comp_name, comp_expr in self.component.iteritems():
+        for (comp_name,tb_name), comp_expr in component.iteritems():
 
             if not os.path.isdir('./AUTO_GEN'):
                 os.makedirs('./AUTO_GEN')
-            tb = open("./AUTO_GEN/auto_"+self.tbname+".ghdl", "a");
-            tb.write(u'''---
+            
+            # Check if testbench already been generated, if yes, delete it.
+            file = "./AUTO_GEN/auto_"+tb_name+".ghdl"
+            if os.path.isfile(file):
+                os.remove(file)
+
+            with open(file, "a+") as tb:
+                # Start writing testbench.
+                tb.write(u'''---
 -- This testbench is automatically generated using a python
 -- script.
 -- (c) Dilawar Singh, dilawar@ee.iitb.ac.in
---
+-- GNU GPL
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.all;
 USE STD.textio.all;
@@ -112,51 +130,54 @@ ENTITY testbench IS
 end ENTITY testbench;
 
 ARCHITECTURE stimulus OF testbench IS\n\tCOMPONENT ''')
-            tb.write(unicode(comp_name)+u'\n')
-            tb.write(u'\tPORT ( \n')
-            tb.write(u'\t'+unicode(comp_expr)+u'\n')
-            tb.write(u'\t);\nEND COMPONENT;\n')
+                tb.write(unicode(comp_name)+u'\n')
+                tb.write(u'\tPORT ( \n')
+                tb.write(u'\t'+unicode(comp_expr)+u'\n')
+                tb.write(u'\t);\nEND COMPONENT;\n')
 
-            # Attach signal.
-            for (name,comp), (a, expr) in self.port.iteritems():
-                if comp == comp_name :
-                    tb.write(u'\tSIGNAL '+unicode(name)+u' : '+unicode(expr))
-                    tb.write(u';\n')
-                else:
-                    pass
+                # Attach signal.
+                for (name,comp), (a, expr) in self.port.iteritems():
+                    if comp == comp_name :
+                        tb.write(u'\tSIGNAL '+unicode(name)+u' : '+unicode(expr))
+                        tb.write(u';\n')
+                    else:
+                        pass
 
-            tb.write(u'BEGIN\n')
-            tb.write(u'\tdut : '+unicode(comp_name)+u' \n\tPORT MAP (');
+                tb.write(u'BEGIN\n')
+                tb.write(u'\tdut : '+unicode(comp_name)+u' \n\tPORT MAP (');
 
-            for (name, comp), (a, expr) in self.port.iteritems():
-                if comp == comp_name :
-                    tb.write(unicode(name)+u', ')
-                else:
-                    pass
-        
-            tb.seek(-2, 2)
-            tb.write(u' );\n')
-            tb.write(u'\n\ttest : PROCESS \n')
+                # construct port map expression.
+                portmap_expr = ''
+                for (name, comp), (a, expr) in self.port.iteritems():
+                    if comp == comp_name :
+                        portmap_expr = portmap_expr+(name)+u', '
+                    else:
+                        pass
+            
+                # remove last comma from the list.
+                expr = portmap_expr[:(len(portmap_expr)-2)]
+                tb.write(unicode(expr)+u' );\n')
+                tb.write(u'\n\ttest : PROCESS \n')
 
-            # Create variables.
-            for (name,comp), (a, expr) in self.port.iteritems():
-                if comp == comp_name :
-                    tb.write(u'\t\tVARIABLE var_'+unicode(name.strip())+u' : '+unicode(expr))
-                    tb.write(u';\n')
-                else:
-                    pass
+                # Create variables.
+                for (name,comp), (a, expr) in self.port.iteritems():
+                    if comp == comp_name :
+                        tb.write(u'\t\tVARIABLE var_'+unicode(name.strip())+u' : '+unicode(expr))
+                        tb.write(u';\n')
+                    else:
+                        pass
 
-            tb.write(u'\n\t\tfile vector_file : text is in \"'\
-                    +unicode(self.tbname)+u'.txt\";\n')
-            tb.write(u'''
+                tb.write(u'\n\t\tfile vector_file : text is in \"'\
+                        +unicode(self.tbname)+u'.txt\";\n')
+                tb.write(u'''
 \t\tVARIABLE l : LINE;
 \t\tVARIABLE vector_time : TIME;
 \t\tVARIABLE r : REAL;
 \t\tVARIABLE good_number, good_val : BOOLEAN;
 \t\tVARIABLE space : CHARACTER;
-                    ''')
+                        ''')
 
-            tb.write(u'''
+                tb.write(u'''
 \t\tBEGIN
 \t\tWHILE NOT endfile(vector_file) LOOP
 \t\t\treadline(vector_file, l);
@@ -167,39 +188,39 @@ ARCHITECTURE stimulus OF testbench IS\n\tCOMPONENT ''')
 \t\t\t\tWAIT FOR vector_time - now;
 \t\t\tEND IF;
 \t\t\tread(l, space);\n''')
-            # read other variables and use them as input.
-            for (name,comp), (a, expr) in self.port.iteritems():
-                if comp == comp_name :
-                    tb.write(u'\t\t\tread(l, var_'+unicode(name.strip())+\
-                            u', good_val);\n') 
-                    tb.write(u'\t\t\tASSERT good_val REPORT \"bad '+\
-                            unicode(name)+u' value\";\n')
-                else:
-                    pass
-            # Assign these values to input ports.
-            for (name,comp), (type, expr) in self.port.iteritems():
-                if comp == comp_name :
-                    if type.strip() == 'in' :
-                        tb.write(u'\t\t\t'+unicode(name)+u' <= var_'+\
-                                unicode(name.strip())+u';\n')
-                else:
-                    pass
+                # read other variables and use them as input.
+                for (name,comp), (a, expr) in self.port.iteritems():
+                    if comp == comp_name :
+                        tb.write(u'\t\t\tread(l, var_'+unicode(name.strip())+\
+                                u', good_val);\n') 
+                        tb.write(u'\t\t\tASSERT good_val REPORT \"bad '+\
+                                unicode(name)+u' value\";\n')
+                    else:
+                        pass
+                # Assign these values to input ports.
+                for (name,comp), (type, expr) in self.port.iteritems():
+                    if comp == comp_name :
+                        if type.strip() == 'in' :
+                            tb.write(u'\t\t\t'+unicode(name)+u' <= var_'+\
+                                    unicode(name.strip())+u';\n')
+                    else:
+                        pass
 
-            # ASSERT OUTPUT ports.
-            for (name,comp), (type, expr) in self.port.iteritems():
-                if comp == comp_name :
-                    if type.strip() == 'out' :
-                        tb.write(u'\t\t\tASSERT var_'+unicode(name.strip())+\
-                                u' = '+unicode(name.strip())+\
-                                u' REPORT \"vector mismatch\"'+\
-                                u' SEVERITY WARNING;\n')
-                else:
-                    pass
+                # ASSERT OUTPUT ports.
+                for (name,comp), (type, expr) in self.port.iteritems():
+                    if comp == comp_name :
+                        if type.strip() == 'out' :
+                            tb.write(u'\t\t\tASSERT var_'+unicode(name.strip())+\
+                                    u' = '+unicode(name.strip())+\
+                                    u' REPORT \"vector mismatch\"'+\
+                                    u' SEVERITY WARNING;\n')
+                    else:
+                        pass
 
-            tb.write(u'\t\tEND LOOP;\n')
-            tb.write(u'\t\tASSERT false REPORT \"Test complete\";\n')
-            tb.write(u'\t\tWAIT;\n')
-            tb.write(u'\t\tEND PROCESS;\n')
-            tb.write(u'\tEND ARCHITECTURE;\n')
+                tb.write(u'\t\tEND LOOP;\n')
+                tb.write(u'\t\tASSERT false REPORT \"Test complete\";\n')
+                tb.write(u'\t\tWAIT;\n')
+                tb.write(u'\tEND PROCESS;\n')
+                tb.write(u'END ARCHITECTURE;\n')
 
 
