e976bdae801409f7e93936edaf0b2fea34096888
dilawar <dilawar.iitb@gmail.com>
2012-04-14 18:53:02 +0530

    Now we can list all files in an directory.

--- HaskellMoodle/get_data.hs
+++ HaskellMoodle/get_data.hs
@@ -1,5 +1,6 @@
 import System.Directory
-import Control.Monad
+import System.FilePath ((</>))
+import Control.Monad (forM, liftM)
 import qualified Data.Map as M
 
 homeDir = "/home/dilawar/Works/myrepo/Courses/VLSIDesignLab/Assignment-1 Submission/"
@@ -25,10 +26,10 @@ studentList1 dir =
                 return Nothing
         
 
-stList = studentList1 homeDir
+stList = do (studentList1 homeDir >>= \(Just list) -> return list)
 
+{-
 -- Now for each student we need to get all files and save them in a dictionary
---getStudentFiles :: Maybe (IO [FilePath]) -> IO [(FilePath, [FilePath])]
 getStudentFiles dirs = do 
     setCurrentDirectory homeDir
     ds <- dirs 
@@ -43,5 +44,26 @@ getStudentFiles dirs = do
                     files <- getDirectoryContents x
                     return $ Just [(x, files)]
                     getStudentFiles (return (Just xs))
+-}
 
+getContentRecursively topDir = do 
+    names <- getDirectoryContents topDir
+    let properNames = filter (`notElem` [".", ".."]) names 
+    paths <- forM properNames $ \name -> do 
+        let path = topDir </> name 
+        isDirectory <- doesDirectoryExist path 
+        if isDirectory 
+            then getContentRecursively path 
+            else return [path]
+    return (concat paths)
 
+getStudentFiles topDir = do 
+    names <- stList
+    let properNames = filter (`notElem` [".", ".."]) names
+    paths <- forM properNames $ \dir -> do 
+        let path = topDir </> dir 
+        isDirectory <- doesDirectoryExist path 
+        if isDirectory 
+            then getContentRecursively path 
+            else return [path]
+    return (concat paths)
