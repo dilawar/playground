0fa6ba06d92fdd9d0596484c9465e9a802fac454
Pyro Villainicus <dilawar.iitb@gmail.com>
2012-10-20 12:50:54 +0530

    It compares exact no of programs.

--- HaskellMoodle/Main.hs
+++ HaskellMoodle/Main.hs
@@ -7,7 +7,6 @@ homeDir = "/home/dilawar/Works/hpc21/2012ee677/Assignment01/Submissions"
 
 main = do
   myPrograms <- listPrograms [".py"] homeDir
-  print myPrograms
   let result = CP.processPrograms myPrograms
   print (length result)
   putStrLn "Done"
