684237301d8ecc94f50f7593e6d9695f71b07487
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-05 22:22:08 +0530

    demo.vhd without component architecture

--- Courses/SystemDesign/VHDL/GHDLDemo/demo.vhd
+++ Courses/SystemDesign/VHDL/GHDLDemo/demo.vhd
@@ -1,8 +1,10 @@
 -- These are standard libraries one HAS TO include. 
 LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL
+USE IEEE.STD_LOGIC_1164.ALL;
+
 -- WORL library is where all of your current designs are stored. 
 -- It is useful to include it.
+
 USE WORK.ALL;
 
 -- VHDL does not differentiate between lower-case and upper-case words. 
@@ -11,6 +13,49 @@ ENTITY dff IS
         a, b : IN BIT;
         reset : IN BIT;
         clk : IN BIT;
-        out   : OUT BIT
+        q   : OUT BIT
       );
 END ENTITY dff;
+
+-- Structural architecture
+ARCHITECTURE structural OF dff IS 
+  -- Declare components.
+  COMPONENT and2
+    port(in1, in2 : in bit;
+        out1 : out bit
+      );
+  end COMPONENT;
+
+  COMPONENT or2
+    port(in1, in2 : in bit;
+         out1 : out bit
+       );
+  end COMPONENT;
+
+  COMPONENT not1 
+    port(in1 : in bit;
+        out1 : out bit 
+      );
+  end COMPONENT;
+
+  COMPONENT dff 
+    port(d : IN BIT;
+        clk : IN BIT;
+        reset : IN BIT;
+        q : OUT BIT
+      );
+  END COMPONENT;
+
+  -- Local signal (like local variables in C).
+  signal andOut, notOut : bit;
+  signal orOut : bit;
+
+BEGIN 
+  -- Now inside the architecture, describe the structure of model.
+  A1 : and2 port map(a, b, andOut);
+  N1 : not1 port map(b, notOut);
+  O1 : or2 port map(andOut, notOut, orOut);
+  D1 : dff port map(orOut, clk, reset, q);
+
+END structural;
+
