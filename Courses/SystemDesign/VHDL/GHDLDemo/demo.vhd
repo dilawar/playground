5e116b91209c1127938850b61d8fbfe0748b48a1
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-05 23:11:15 +0530

    Updated VHDL file.

--- Courses/SystemDesign/VHDL/GHDLDemo/demo.vhd
+++ Courses/SystemDesign/VHDL/GHDLDemo/demo.vhd
@@ -8,18 +8,21 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE WORK.ALL;
 
 -- VHDL does not differentiate between lower-case and upper-case words. 
-ENTITY dff IS 
+ENTITY demoEntity IS 
   PORT(
         a, b : IN BIT;
         reset : IN BIT;
         clk : IN BIT;
         q   : OUT BIT
       );
-END ENTITY dff;
+END ENTITY;
 
 -- Structural architecture
-ARCHITECTURE structural OF dff IS 
-  -- Declare components.
+ARCHITECTURE structural OF demoEntity IS 
+  -- Declare components. These components have entity-architecture pairs 
+  -- declared some other file (or may be in this file also). 
+  -- We can also use already available components in some library like we 
+  -- use functions available in some library.
   COMPONENT and2
     port(in1, in2 : in bit;
         out1 : out bit
@@ -51,7 +54,9 @@ ARCHITECTURE structural OF dff IS
   signal orOut : bit;
 
 BEGIN 
-  -- Now inside the architecture, describe the structure of model.
+  -- Now inside the architecture, describe the structure of model. 
+  -- Port map declares which port is connected with what.
+  -- There is one more style to map the ports.
   A1 : and2 port map(a, b, andOut);
   N1 : not1 port map(b, notOut);
   O1 : or2 port map(andOut, notOut, orOut);
@@ -59,3 +64,15 @@ BEGIN
 
 END structural;
 
+-- behavioural architecture
+ARCHITECTURE behav of demoEntity IS 
+BEGIN
+PROCESS(clk, reset) 
+  BEGIN 
+    if(reset = '1') then 
+      q <= '0';
+    elsif(clk'event and clk='1') then 
+      q <= ( a and b) or (not b);
+    end if;
+  END PROCESS;
+END behav;
