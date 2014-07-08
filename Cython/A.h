79c1c012f15260958b4bcc9c18d0b4f01c8e0f6c
Dilawar <dilawars@iitb.ac.in>
2014-07-09 02:18:33 +0530

    Testing what I can do with cython.

---
+++ Cython/A.h
@@ -0,0 +1,34 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  A.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Wednesday 09 July 2014 02:15:19  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawar@ee.iitb.ac.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+    A()
+    {
+        ;
+    }
+
+    void printA()
+    {
+        cerr << "A " << endl;
+    }
+};
---
+++ Cython/B.h
@@ -0,0 +1,36 @@
+/*
+ * ==============================================================================
+ *
+ *       Filename:  B.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Wednesday 09 July 2014 02:16:29  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawar@ee.iitb.ac.in
+ *   Organization:  
+ *
+ * ==============================================================================
+ */
+
+#include <iostream>
+
+class A;
+
+using namespace std;
+
+class B
+{
+    B()
+    {
+        ;
+    }
+
+    void printB(A& a)
+    {
+        cerr << "B " << endl;
+    }
+};
