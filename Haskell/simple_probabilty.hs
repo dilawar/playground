6219f112b365f90aafd3dd6eb3dfd1e90063919d
Dilawar <dilawars@iitb.ac.in>
2014-09-03 00:43:30 +0530

    Added a file which computes simple probability.

--- Haskell/simple_probabilty.hs
+++ Haskell/simple_probabilty.hs
@@ -1,8 +1,29 @@
-import qualified Numeric.Probability.Distribution as Dist
+import Numeric.Probability.Distribution 
 import qualified Numeric.Probability.Transition as Tran
 import qualified Numeric.Probability.Random as Random
 import qualified Numeric.Probability.Object as Obj
 import qualified System.Random as Rnd
 
+die = uniform [1..6]
+
+-- What is the probability of getting 2.
+prob2 = (??) (==2) die
+
+-- What is the probability of getting number n
+prob n | n < 0 = error "Need a positive int value"
+       | otherwise = (??) (==n) die
 
 
+------
+-- Now another example, lets create two indepedent die.
+
+die1 = die
+die2 = die
+
+-- Now let's join these two distributions.
+
+
+
+-- Roll a dice n times
+{-dice :: Dist [Int]-}
+dice 0 = certainly []
