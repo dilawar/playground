85294d2c92050fc698130bd7a97c1f492edb77d3
Dilawar Singh <dilawars@ncbs.res.in>
2015-03-28 10:25:31 +0530

    Fixed a bug.

--- Python/for_aridni/query.py
+++ Python/for_aridni/query.py
@@ -21,9 +21,10 @@ from collections import defaultdict
 
 db = list()
 found = defaultdict(set)
+conditions = defaultdict()
 
 
-def query(qFile, dbFile):
+def query(qFile, dbFile, conditionFile):
     with open(dbFile, "r") as dbF:
         print("[INFO] Opening db file %s" % dbFile)
         for i, line in enumerate(dbF):
@@ -44,17 +45,47 @@ def query(qFile, dbFile):
                         print("For query %s, %s found" % (qu, s))
                         found[q].add(s)
 
+    with open(conditionFile, "r") as condF:
+        cond = csv.reader(condF, dialect='excel')
+        for row in cond:
+            genes = row[0].split(',')
+            for gene in genes:
+                conditions[gene] = row[1:]
+                #newRows.append([gene] + row[1:])
+
+
     with open('match.csv', 'w') as outF:
         outF.write("query,results\n")
         for k in found:
-            outF.write(k+",")
-            outF.write(",".join(found[k]))
-            outF.write("\n")
+            # Check condition
+            newFound = checkCondition(k)
+            print newFound
+            if newFound:
+                outF.write(k+",")
+                outF.write(",".join(newFound))
+                outF.write("\n")
+
+def checkCondition(gene):
+    print("Checking for %s" % gene)
+    founds = found[gene]
+    newFound = []
+    for f in founds:
+        c = conditions.get(f.upper(), [])
+        if not c:
+            newFound.append(f)
+        else:
+            control, treatment, foldChange = [float(x) for x in c]
+            if control > 0.5 or treatment > 0.5:
+                if foldChange > 1.5 or foldChange < -0.6:
+                    newFound.append(f)
+    return newFound
+
 
 def main():
     dbFile = sys.argv[1]
     queryFile = sys.argv[2]
-    query(queryFile, dbFile)
+    conditionFile = sys.argv[3]
+    query(queryFile, dbFile, conditionFile)
 
 if __name__ == '__main__':
     main()
--- Python/for_aridni/run.sh
+++ Python/for_aridni/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./query.py ./sheets/lymph_tf_db.csv ./sheets/lymph_tf_query.csv
+python ./query.py ./sheets/lymph_tf_db.csv ./sheets/lymph_tf_query.csv ./sheets/conditions.csv
