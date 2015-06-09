11bcd41324a5d56543e6d1f455e0b2a9a600b2f8
Dilawar Singh <dilawars@ncbs.res.in>
2015-06-09 21:20:27 +0530

    Two dice distribution can be computed.

--- Haskell/simple_probabilty.hs
+++ Haskell/simple_probabilty.hs
@@ -3,7 +3,13 @@ import qualified Numeric.Probability.Transition as Tran
 import qualified Numeric.Probability.Random as Random
 import qualified Numeric.Probability.Object as Obj
 import qualified System.Random as Rnd
+import Control.Monad (liftM2)
+import qualified Data.Map as M
 
+type Die = Int
+type Dist = Dist.T Rational
+
+die :: Dist Die
 die = Dist.uniform [1..6]
 
 -- What is the probability of getting 2.
@@ -22,8 +28,6 @@ die2 = die
 
 -- Now let's join these two distributions.
 
-
-
 -- Roll a dice n times
 {-dice :: Dist [Int]-}
 dice 0 = Dist.certainly []
@@ -34,3 +38,25 @@ entropy dist = sum $ map( \x -> - x * logBase 2 x ) propabilities
         listOfdistribution = Dist.decons dist 
         events = Prelude.map (\x -> fst x) listOfdistribution
         propabilities = Prelude.map (\x -> snd x) listOfdistribution
+
+-- Create two dice
+twoDice :: Dist (Die, Die)
+twoDice = liftM2 (,) die die
+
+-- Now we can compute the probability distribution of getting various outcomes
+-- i.e. Pr(S = s) where s \in [2,3..12].
+
+oneDieSampleSpace :: [Die]
+oneDieSampleSpace = [1..6]
+
+twoDiceSampleSpace :: [(Die, Die)]
+twoDiceSampleSpace = [ (x, y) | x <- oneDieSampleSpace, y <- oneDieSampleSpace ]
+
+probilities = map (\x -> (fst x+snd x,(==x) Dist.?? twoDice)) twoDiceSampleSpace
+
+-- This function computes the distribution of outcome when two fair dice are
+-- thrown.
+twoDiceDistribution = helper probilities M.empty 
+    where 
+        helper (x:xs) dist = helper xs $ M.insertWith (+) (fst x) (snd x) dist
+        helper [] dist = dist
