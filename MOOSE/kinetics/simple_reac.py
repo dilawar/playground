386cb437e51299e201685b1cc5ab99b42caa381d
Dilawar Singh <dilawars@ncbs.res.in>
2016-01-20 17:35:32 +0530

    This works. Nothing is wrong with MOOSE.

--- MOOSE/kinetics/simple_reac.py
+++ MOOSE/kinetics/simple_reac.py
@@ -1,5 +1,7 @@
 import moose
 import moose.utils as mu
+import pylab
+import numpy as np
 
 compts_ = {}
 tables_ = {}
@@ -8,13 +10,13 @@ def add_molecule( name, num ):
     p = moose.Pool( '/compt/%s' % name )
     p.concInit = num
     t = moose.Table2( '/table%s' % name )
-    moose.connect( t, 'requestOut', p, 'getConc' )
+    moose.connect( t, 'requestOut', p, 'getN' )
     tables_[name] = t
     compts_[name] = p
     return p
 
 compt = moose.CubeMesh('/compt')
-compt.volume = 1
+compt.volume = 1e-20
 
 a = add_molecule( 'a', 1e-1 )
 b = add_molecule( 'b', 2e-1 )
@@ -37,4 +39,12 @@ stoich.path = '/compt/##'
 moose.reinit()
 moose.start(100)
 
-mu.plotRecords( tables_ )
+vectors = []
+pylab.subplot(2, 1, 1)
+for t in tables_:
+    pylab.plot( tables_[t].vector )
+    vectors.append( tables_[t].vector )
+
+pylab.subplot(2, 1, 2)
+pylab.plot( np.sum( vectors, axis = 0 ))
+pylab.show( )
