1cf67a47a9d2400ae0ae09cf49310c66d0103cee
Dilawar Singh <dilawars@ncbs.res.in>
2017-08-30 18:00:40 +0530

    Finish at home.

--- CS/compressed_sensing.py
+++ CS/compressed_sensing.py
@@ -1,14 +1,3 @@
-"""compressed_sensing.py: 
-
-"""
-    
-__author__           = "Dilawar Singh"
-__copyright__        = "Copyright 2017-, Dilawar Singh"
-__version__          = "1.0.0"
-__maintainer__       = "Dilawar Singh"
-__email__            = "dilawars@ncbs.res.in"
-__status__           = "Development"
-
 import sys
 import os
 import matplotlib.pyplot as plt
