a93b50930ed47c2429aa7910d6f9567d47640691
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-03 14:05:18 +0530

    Random numbers in boost and c++.

--- CLike/random_boost_std.cpp
+++ CLike/random_boost_std.cpp
@@ -9,16 +9,17 @@ int main(int argc, const char *argv[])
 {
 
     // Initialize seed .
-    std::mt19937 gena( 1 );
-    boost::random::mt19937 genb( 1 );
+    std::mt19937 gena;
+    boost::random::mt19937 genb;
 
     std::uniform_real_distribution<> dista(0, 1);
     boost::random::uniform_01<> distb;
     boost::random::uniform_real_distribution<> distc(0,1);
     
 
-    double ra, rb = 0.0;
-    for (size_t i = 0; i < 10; i++) 
+    // double ra, rb = 0.0;
+    std::cout << "STD BOOST1 BOOST2" << std::endl;
+    for (size_t i = 0; i < 20; i++) 
         std::cout << dista(gena) << ' ' << distb(genb) << ' ' << distc(genb) << std::endl;
 
     return 0;
