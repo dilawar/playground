2a096f62d3b6a57b4b6eee16ba737f33b1d9b450
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-09 19:31:27 +0530

    Table is being downloaded fine.

--- Scripts/download_all_kml_selenium.py
+++ Scripts/download_all_kml_selenium.py
@@ -17,22 +17,46 @@ baseUrl_ = 'http://egreenwatch.nic.in/Public/Reports/View_Download_KML.aspx'
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
+profile = FirefoxProfile()
+profile.set_preference("browser.download.panel.shown", False);
+ftype = 'application/octet-stream'
+profile.set_preference("browser.helperApps.neverAsk.openFile", ftype )
+profile.set_preference("browser.helperApps.neverAsk.saveToDisk", ftype)
+profile.set_preference("browser.download.folderList", 2); 
+profile.set_preference("browser.download.dir", "./");
+profile.set_preference("browser.helperApps.alwaysAsk.force", False);
+profile.set_preference("browser.download.manager.useWindow", False);
+
 options = Options()
 options.add_argument("--headless")
-driver = webdriver.Firefox(firefox_options=options)
+driver = webdriver.Firefox(firefox_options=options, firefox_profile=profile)
+
 print("[INFO] Firefox Headless Browser Invoked")
 import time
 
 downloadBtnID = 'ctl00_ctl00_dpPH_dpPH_btnSearch'
 tableId = 'ctl00_ctl00_dpPH_dpPH_gdCALs'
 states_ = []
+current_page_ = 1
 
 def find_select_by_id( id ):
     global driver
     ss = driver.find_element_by_id( id )
     return Select(ss)
+
+def download_from_table( table ):
+    ds = table.find_elements_by_xpath( '//input[@text="Download"]' )
+    for i, d in enumerate(ds):
+        try:
+            d.click()
+            print( 'Downloadin by pressing button ID: %s' % d.get_attribute( 'id' ) )
+        except Exception as e:
+            print( 'Coud not click: %s' % e )
+            pass
+
 
 def download_kmp( state, siteType):
     global driver
@@ -52,12 +76,28 @@ def download_kmp( state, siteType):
     time.sleep(1)
 
     # now download verything on this page.
-    trs = driver.find_elements_by_xpath( '//table[@id="%s"]//tr' % tableId )
-    for i, tr in enumerate(trs):
-        #  print( i, tr.get_attribute( 'innerHTML') )
-        d = tr.find_element_by_xpath( '//input[@text="Download"]' )
-        print( i, d )
-        d.click()
+    #  pageLink = "javascript:__doPostBack('ctl00$ctl00$dpPH$dpPH$gdCALs','Page$%d')"
+    table = driver.find_element_by_xpath( '//table[@id="%s"]' % tableId )
+    download_from_table( table )
+
+    return
+
+    assert table, "Table not found"
+    pages = table.find_elements_by_xpath( '//td/a' )
+
+    #  print( table.get_attribute( 'innerHTML' ) )
+    for p in pages:
+        href = p.get_attribute( 'href')
+        if 'Page$' not in href:
+            continue
+        try:
+            print( 'Trying page', p )
+            p.click()
+        except Exception as e:
+            print( e )
+            continue
+
+        continue
 
 def main( ):
     global driver
