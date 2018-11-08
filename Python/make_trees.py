826627536443e2669fca1bcd9ab9f4c79a37b986
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-08 17:23:02 +0530

    update to axis limits.

--- Python/make_trees.py
+++ Python/make_trees.py
@@ -30,12 +30,17 @@ def construct_Tree(i):
 	colors = [G[u][v]['color'] for u,v in edges]
 	weights = [G[u][v]['weight'] for u,v in edges]
 	colors_n = ['teal','crimson', 'gold', 'mediumblue', 'darkgreen', 'darkviolet','orangered', 'pink']
+
+        # First fix the axis lower limits.
 	nx.draw_networkx(G, pos=pos, edges=edges
                 , with_labels = False
                 , node_color=colors_n
                 , edge_color=colors
                 , width=weights
                 )
+
+        plt.xlim( left = 0 )
+        plt.ylim( bottom = 0 )
         # Uncomment to remove the axis
         #  plt.axis('off')
 
