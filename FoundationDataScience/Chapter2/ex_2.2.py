291ae25a63f326922f02087ec21d80739f8b868f
Dilawar Singh <dilawars@ncbs.res.in>
2019-10-03 23:15:51 +0530

    Fixed the script.

--- FoundationDataScience/Chapter2/ex_2.2.py
+++ FoundationDataScience/Chapter2/ex_2.2.py
@@ -10,14 +10,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 
-pts = np.random.random(size=(30,1000)) - 0.5
+pts = np.random.random(size=(30, 100)) - 0.5
 
-for p1, p2 in itertools.product(pts, repeat=2):
-    d = sum(p1**2 - p2**2)**0.5
+for p1, p2 in itertools.combinations(pts, 2):
+    d = sum((p1 - p2)**2)**0.5
     cosine = np.dot(p1, p2)/np.linalg.norm(p1, 2)/np.linalg.norm(p2, 2)
-    if d != 0:
-        print(d, 180*math.acos(cosine)/math.pi)
-    else:
-        print(0, 90)
+    print(d, 180*math.acos(cosine)/math.pi)
 
 
