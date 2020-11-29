99080426d9d5f6b7e269dd8578f69c9604babe50
Dilawar Singh <dilawar.s.rajput@gmail.com>
2020-11-29 10:38:36 +0530

    sigmoid is done.

--- Coursera/ML/machine-learning-ex2/ex2/plotData.m
+++ Coursera/ML/machine-learning-ex2/ex2/plotData.m
@@ -11,7 +11,13 @@ figure; hold on;
 %               2D plot, using the option 'k+' for the positive
 %               examples and 'ko' for the negative examples.
 %
+success = find(y==1);
+failed = find(y==0);
 
+plot(X(success, 1), X(success, 2), 'k+', 2, 'MarkerSize', 7);
+plot(X(failed, 1), X(failed, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+xlabel("Exam 1 score");
+ylabel("Exam 2 score");
 
 
 
