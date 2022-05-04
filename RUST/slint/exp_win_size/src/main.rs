f81d7f69f941194a93d9ff44a20e3bbe7eade1a5
Dilawar Singh <dilawar@subcom.tech>
2022-05-04 15:54:46 +0530

    chore: Added link to discussion

--- RUST/slint/exp_win_size/src/main.rs
+++ RUST/slint/exp_win_size/src/main.rs
@@ -4,6 +4,9 @@ slint::slint!{
     HelloWorld := Window {
         width: 100px;
         height: 100px;
+        // https://github.com/slint-ui/slint/discussions/1229
+        property window_width <=> width;
+        property window_height <=> height;
         Text {
             text: "hello world";
             color: green;
@@ -13,6 +16,6 @@ slint::slint!{
 
 fn main() {
     let win = HelloWorld::new();
-    println!("width of window is {}", win.get_width());
+    println!("width of window is {}", win.get_window_width());
     win.run();
 }
