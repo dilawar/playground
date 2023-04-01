39c0ce088d71fbed1c9ca60d48965722dc4d76c2
Dilawar Singh <dilawar@subcom.tech>
2023-04-01 16:24:00 +0530

    feat: Adds a simple algorithm to compute hash of a directory

--- RUST/walk-dir/src/main.rs
+++ RUST/walk-dir/src/main.rs
@@ -1,9 +1,34 @@
 use std::path::Path;
-use fts::walkdir::{WalkDir, WalkDirConf};
+use std::time::Instant;
+use walkdir::WalkDir;
+
+/// Compute a simple hash of a directory and its children.
+fn compute_hash_simple_xor(path: &Path) -> anyhow::Result<u64> {
+    let mut total = 0usize;
+    let mut hash = 0u64;
+    for p in WalkDir::new(path) {
+        if p.is_err() {
+            continue;
+        }
+        total += 1;
+        if let Ok(metadata) = p.unwrap().metadata() {
+            if let Ok(mtime) = metadata.modified() {
+                if let Ok(mtime_since) = mtime.duration_since(std::time::SystemTime::UNIX_EPOCH) {
+                    hash ^= mtime_since.as_secs();
+                }
+            }
+        }
+    }
+    hash ^= total as u64;
+    Ok(hash)
+}
 
 fn main() {
     let path = Path::new("C:/Program Files");
-    for p in WalkDir::new(WalkDirConf::new(path)) {
-        println!("{:?}", p.unwrap());
-    }
+
+    let now = Instant::now();
+    let hash = compute_hash_simple_xor(path);
+    let dt = now.elapsed().as_millis();
+
+    println!("Hash of `{}` is {hash:?}. Took {dt} ms", path.display());
 }
