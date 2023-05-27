8bf3ad464b4ab979feb9b6c5d44d09630e67bfe3
Dilawar Singh <dilawar@subcom.tech>
2023-05-27 13:38:53 +0530

    clone is as good as r

--- RUST/channels/exp_tx_clone/src/main.rs
+++ RUST/channels/exp_tx_clone/src/main.rs
@@ -3,12 +3,44 @@ use std::thread;
 fn main() {
     let (s1, r) = crossbeam_channel::bounded(10);
     let s2 = s1.clone();
+    let r2 = r.clone();
 
-    thread::spawn(move || s1.send(1).unwrap());
-    thread::spawn(move || s2.send(2).unwrap());
+    thread::spawn(move || {
+        s1.send(1).unwrap();
+        s1.send(3).unwrap();
+        s1.send(5).unwrap();
+    });
+    thread::spawn(move || {
+        s2.send(2).unwrap();
+        s2.send(4).unwrap();
+        s2.send(6).unwrap();
+    });
 
-    let msg1 = r.recv().unwrap();
-    let msg2 = r.recv().unwrap();
+    for _i in 0..3 {
+        let msg1 = r.recv().unwrap();
+        let msg2 = r.recv().unwrap();
+        println!("1 {msg1}");
+        println!("1 {msg2}");
+    }
 
-    assert_eq!(msg1 + msg2, 3);
+    let (s1, r) = crossbeam_channel::bounded(10);
+    let s2 = s1.clone();
+    let r2 = r.clone();
+
+    thread::spawn(move || {
+        s1.send(1).unwrap();
+        s1.send(3).unwrap();
+        s1.send(5).unwrap();
+    });
+    thread::spawn(move || {
+        s2.send(2).unwrap();
+        s2.send(4).unwrap();
+        s2.send(6).unwrap();
+    });
+    for _i in 0..3 {
+        let msg1 = r.recv().unwrap();
+        let msg2 = r2.recv().unwrap();
+        println!("2 {msg1}");
+        println!("2 {msg2}");
+    }
 }
--- SoundDevices/pocket_sphinx.py
+++
@@ -1,4 +0,0 @@
-from pocketsphinx import LiveSpeech
-
-for phrase in LiveSpeech( ):
-    print( phrase )
