f9692b89806d16958613cf62d7effa4723fd210b
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-07-27 21:53:42 +0530

    Fixed another bug

--- Leetcode/leetcode-rs/examples/merge_sorted_array.rs
+++ Leetcode/leetcode-rs/examples/merge_sorted_array.rs
@@ -4,25 +4,32 @@ impl Solution {
     pub fn merge_sorted_array(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         nums1.drain(m as usize..);
         nums1.reserve((m + n) as usize);
-
         let mut idx = 0;
 
-        while !nums1.is_empty() && idx < (nums1.len() - 1) {
+        // eprintln!("111 nums1={nums1:?}, nums2={nums2:?}.");
+        while !nums1.is_empty() && idx < nums1.len() {
             let a = nums1[idx];
-            let b = nums1[idx + 1];
-            eprintln!("idx={idx} a={a} b={b}");
 
+            // Any element from second vector which is less than a goes before a.
+            let j = nums2.partition_point(|&x| x <= a);
+            for x in nums2.drain(0..j) {
+                nums1.insert(idx, x);
+            }
+            // eprintln!("handing when a={a}, nums1={nums1:?} idx={idx}");
+
+            if nums1.get(idx + 1).is_none() {
+                break;
+            }
+
+            let b = nums1[idx + 1];
             // find slice that is inside nums2 and drain it.
             let j = nums2.partition_point(|&x| x <= b);
+            // eprintln!("idx={idx} j={j} a={a} b={b}");
             if j < nums2.len() {
                 for x in nums2.drain(0..j) {
-                    if x < a {
-                        nums1.insert(idx, x);
-                    } else {
-                        nums1.insert(idx + 1, x);
-                    }
+                    nums1.insert(idx + 1, x);
                     idx += 1;
-                    eprintln!("handing x={x} when a={a}, b={b}, nums1={nums1:?} idx={idx}");
+                    // eprintln!("handing x={x} when a={a}, b={b}, nums1={nums1:?} idx={idx}");
                 }
             }
 
@@ -37,21 +44,29 @@ impl Solution {
 fn main() {
     let inst = std::time::Instant::now();
 
+    println!("1. =======");
     let mut nums1 = vec![1, 2, 3, 0, 0, 0];
     let mut nums2 = vec![2, 5, 6];
     Solution::merge_sorted_array(&mut nums1, 3, &mut nums2, 3);
     assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
 
-    println!("=======");
+    println!("2. =======");
+    nums1 = vec![2, 0];
+    nums2 = vec![1];
+    Solution::merge_sorted_array(&mut nums1, 1, &mut nums2, 1);
+    assert_eq!(nums1, vec![1, 2]);
+
+    println!("3. =======");
     nums1 = vec![1];
     nums2 = vec![];
     Solution::merge_sorted_array(&mut nums1, 1, &mut nums2, 0);
     assert_eq!(nums1, vec![1]);
 
-    println!("=======");
+    println!("4. =======");
     nums1 = vec![0];
     nums2 = vec![1];
     Solution::merge_sorted_array(&mut nums1, 0, &mut nums2, 1);
     assert_eq!(nums1, vec![1]);
+
     println!("Took {:?}", inst.elapsed());
 }
