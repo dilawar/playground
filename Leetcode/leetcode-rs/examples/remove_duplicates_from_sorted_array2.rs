943a68dee3131775bc4d472611d0bd08d3ae7374
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-07-27 19:04:21 +0530

    Could not find a faster solution.

--- Leetcode/leetcode-rs/examples/remove_duplicates_from_sorted_array2.rs
+++ Leetcode/leetcode-rs/examples/remove_duplicates_from_sorted_array2.rs
@@ -4,12 +4,25 @@ impl Solution {
     /// Given an integer array nums sorted in non-decreasing order,
     /// remove some duplicates in-place such that each unique element appears
     /// at most twice. The relative order of the elements should be kept the same.
-    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-        let mut removed = 0usize;
-
+    pub fn remove_duplicates1(nums: &mut Vec<i32>) -> i32 {
         let mut idx = 0usize;
-        let mut N = nums.len();
-        while idx < N {
+        while idx < nums.len() - 2 {
+            let a = nums[idx];
+            let j = nums.partition_point(|&x| x <= a);
+            let step = j - idx;
+            if step <= 2 {
+                idx += step;
+                continue;
+            }
+            nums.drain(idx + 2..j);
+            idx += 2;
+        }
+        nums.len() as i32
+    }
+
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut idx = 0usize;
+        while idx < nums.len() - 2 {
             let a = nums[idx];
 
             // These are optimization to speed things up.
@@ -28,13 +41,10 @@ impl Solution {
 
             // Remove more than two consecutive duplicates.
             let j = nums.partition_point(|&x| x <= a);
-            eprintln!("idx={idx}, a={a}, N={N}, j={j}, {nums:?}");
+            // eprintln!("idx={idx}, a={a}, N={N}, j={j}, {nums:?}");
             if (idx + 2) < j {
                 nums.drain(idx + 2..j);
-                removed += j - idx - 2;
-                eprintln!("range {}..{j}, #removed={removed}", idx + 2);
-
-                N = nums.len();
+                // eprintln!("removed range {}..{j}", idx + 2);
                 idx += 2;
             } else {
                 idx += 1;
@@ -45,22 +55,27 @@ impl Solution {
 }
 
 fn main() {
-    let mut num = vec![1, 1, 1];
-    assert_eq!(Solution::remove_duplicates(&mut num), 2);
-    assert_eq!(num, vec![1, 1]);
+    let inst = std::time::Instant::now();
+    for i in 0..10000 {
+        let mut num = vec![1, 1, 1];
+        assert_eq!(Solution::remove_duplicates(&mut num), 2);
+        assert_eq!(num, vec![1, 1]);
 
-    println!("===========");
-    num = vec![1, 2, 2, 2];
-    assert_eq!(Solution::remove_duplicates(&mut num), 3);
-    assert_eq!(num, vec![1, 2, 2]);
+        // println!("===========");
+        num = vec![1, 2, 2, 2];
+        assert_eq!(Solution::remove_duplicates(&mut num), 3);
+        assert_eq!(num, vec![1, 2, 2]);
 
-    println!("===========");
-    num = vec![1, 1, 1, 2, 2, 3];
-    assert_eq!(Solution::remove_duplicates(&mut num), 5);
-    assert_eq!(num, vec![1, 1, 2, 2, 3]);
+        // println!("===========");
+        num = vec![1, 1, 1, 2, 2, 3];
+        assert_eq!(Solution::remove_duplicates(&mut num), 5);
+        assert_eq!(num, vec![1, 1, 2, 2, 3]);
 
-    println!("===========");
-    num = vec![0, 0, 1, 1, 1, 1, 2, 3, 3];
-    assert_eq!(Solution::remove_duplicates(&mut num), 7);
-    assert_eq!(num, vec![0, 0, 1, 1, 2, 3, 3]);
+        // println!("===========");
+        num = vec![0, 0, 1, 1, 1, 1, 2, 3, 3];
+        assert_eq!(Solution::remove_duplicates(&mut num), 7);
+        assert_eq!(num, vec![0, 0, 1, 1, 2, 3, 3]);
+    }
+
+    println!("Took {:?}", inst.elapsed());
 }
