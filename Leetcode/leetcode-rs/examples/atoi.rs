e171dbbd84e9e56f8d12ac2d4877bcdea3354147
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-07-29 10:27:10 +0530

    a cleaner solution

--- Leetcode/leetcode-rs/examples/atoi.rs
+++ Leetcode/leetcode-rs/examples/atoi.rs
@@ -1,66 +1,53 @@
+use std::collections::VecDeque;
 pub struct Solution;
 
 impl Solution {
     pub fn my_atoi(s: String) -> i32 {
         let mut result = 0i64;
+
+        // sanitize the string.
+        let mut s = s.trim().as_bytes().iter();
         let mut is_neg = false;
-        let mut is_pos = false;
-        let mut m = 0;
+        let mut digits: VecDeque<u8> = VecDeque::with_capacity(s.len());
 
-        for c in s.as_bytes().iter().rev() {
-            if *c == b' ' {
-                continue;
-                if result > 0 {
-                    break;
-                }
-            }
-
-            // This should be the last character but it can also occur in between.
-            if *c == b'-' {
+        // first character is sign.
+        if let Some(sign_byte) = s.next() {
+            if *sign_byte == b'-' {
                 is_neg = true;
-                if is_pos {
-                    m = 0;
-                    result = 0;
+            } else if *sign_byte == b'+' {
+                is_neg = false;
+            } else {
+                let v = *sign_byte - b'0';
+                if v <= 9 {
+                    digits.push_back(v);
+                } else {
+                    eprintln!("invalid digit or sign {sign_byte}");
+                    return result as i32;
                 }
-                continue;
             }
-            if *c == b'+' {
-                is_pos = true;
-                if is_neg {
-                    m = 0;
-                    result = 0;
-                }
-                continue;
-            }
+        }
 
+        while let Some(c) = s.next() {
+            if *c < b'0' || *c > b'9' {
+                eprintln!("{c} is not a valid digit...");
+                break;
+            }
             let v = *c - b'0';
-            if v > 9 {
-                eprintln!("{c} is not a digit. Reset!");
-                result = 0;
-                m = 0;
-                is_neg = false;
-                continue;
-            }
+            eprintln!(" c={c} v={v} ");
+            digits.push_back(v);
+        }
 
-            // valid digit, check if - came before it. If yes, then ignore it.
-            if (is_neg || is_pos) {
-                eprintln!("-/+ was seen before. Ignoring previous result.");
-                m = 0;
-                result = 0;
-                is_neg = false;
-                is_pos = false;
-            }
-
-            result += (v as i64 * 10_i64.pow(m));
+        for (m, v) in digits.iter().rev().enumerate() {
+            result += (*v as i64) * 10_i64.pow(m as u32);
             result = result.min(i64::MAX);
-            eprintln!(" value = {v}, result= {result}.");
-            m += 1;
+            eprintln!(" v={v} m={m} result = {result}");
         }
 
         if is_neg {
             result = -result;
         }
 
+        eprintln!("\n=== result is {result}");
         if result > (i32::MAX as i64) {
             i32::MAX
         } else if result < (i32::MIN as i64) {
