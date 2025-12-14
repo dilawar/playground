3b27c80fc6ce29725ec0f64a86b6017c99dea7d2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2025-12-14 22:31:32 +0530

    not working LRU cache

--- Leetcode/leetcode-cpp/lru/lru.cpp
+++ Leetcode/leetcode-cpp/lru/lru.cpp
@@ -2,18 +2,21 @@
 #include <cassert>
 #include <chrono>
 #include <functional>
-#include <ios>
 #include <iostream>
 #include <map>
+
+using std::cout;
+using std::endl;
 
 class LRUCache {
 public:
   LRUCache(int capacity) : capacity(capacity), pCache({}) {}
 
   int get(int key) {
-    std::cout << " pCache size: " << pCache.size() << std::endl;
-    if (auto found = pCache.find(key) != pCache.end()) {
-      return pCache[key].second;
+    std::cout << "get '" << key << "' " <<  " cache size=" << pCache.size() << std::endl;
+    if (auto found = pCache.find(key); found != pCache.end()) {
+      cout << " found it: " << found->second.second << endl;
+      return found->second.second;
     }
     return -1;
   }
@@ -21,9 +24,12 @@ public:
   void put(int key, int value) {
     // todo: check capacity and eviction
     if (pCache.size() >= capacity) {
-      printf("eviction");
+      // printf("eviction");
       // find the least recently used key
-      std::vector<std::pair<long, int>> new_vec = {};
+      std::vector<std::pair<long, int>> new_vec;
+      new_vec.reserve(capacity);
+      
+      // transform 
       std::transform(pCache.cbegin(), pCache.cend(), new_vec.begin(),
                      [](auto const &x) -> std::pair<long, int> {
                        return {x.second.first, x.first};
@@ -42,17 +48,41 @@ public:
     pCache[key] = std::make_pair(now, value);
   }
 
+  friend std::ostream &operator<<(std::ostream &os, LRUCache const &obj) {
+    os << "[";
+    for(auto i: obj.pCache)
+    {
+      os << i.first << "=" << i.second.second << ",";
+    }
+    os << "]" << endl;
+    return os;
+  }
+
 private:
   int capacity;
   std::map<int, std::pair<long, int>> pCache;
 };
 
+
 int main(int argc, char const *argv[]) {
   // Your LRUCache object will be instantiated and called as such:
-  LRUCache *obj = new LRUCache(10);
-  auto param_1 = obj->get(10);
-  assert(param_1 == -1);
-  obj->put(10, 9);
-  param_1 = obj->get(10);
-  assert(param_1 == 9);
+  LRUCache *obj = new LRUCache(2);
+  obj->put(1, 1);
+  cout << *obj << endl;
+  obj->put(2, 2);
+  cout << *obj << endl;
+  obj->get(1);
+  cout << *obj << endl;
+  obj->put(3, 3);
+  cout << *obj << endl;
+  obj->get(2);
+  cout << *obj << endl;
+  obj->put(4, 4);
+  cout << *obj << endl;
+  obj->get(1);
+  cout << *obj << endl;
+  obj->get(3);
+  cout << *obj << endl;
+  obj->get(4);
+  cout << *obj << endl;
 }
